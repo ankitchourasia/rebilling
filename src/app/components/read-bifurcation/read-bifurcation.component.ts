fd41a00b907ed1998a691066f24b16ad5ff66c5d
unknown <ankit.chourasia@outlook.com>
2023-10-09 18:05:11 +0530

    bifurcation in progress

---
+++ src/app/components/read-bifurcation/read-bifurcation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-read-bifurcation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './read-bifurcation.component.html',
+  styleUrls: ['./read-bifurcation.component.css']
+})
+export class ReadBifurcationComponent {
+
+}
