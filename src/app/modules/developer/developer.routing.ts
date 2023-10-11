7b24d550a059978a679ea927dab8ed62f9294667
unknown <ankit.chourasia@outlook.com>
2023-10-11 17:43:51 +0530

    consumption bifurcation included in developer login

--- src/app/modules/developer/developer.routing.ts
+++ src/app/modules/developer/developer.routing.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DeveloperHomeComponent } from './developer-home/developer-home.component';
 import { DeveloperComponent } from './developer.component';
 import { DeveloperValidateReadingComponent } from './developer-validate-reading/developer-validate-reading.component';
+import { ConsumptionReportComponent } from 'src/app/components/consumption-report/consumption-report.component';
 
 const developerRoutes: Routes = [
   {
@@ -15,6 +16,10 @@ const developerRoutes: Routes = [
       {
         path: 'read-validate', 
         component: DeveloperValidateReadingComponent
+      },
+      {
+        path: 'read-bifurcation', 
+        component: ConsumptionReportComponent
       },
       {
         path: '', redirectTo: 'home', pathMatch: 'full'
